10f9786cb81b5d36964efd7816fdf198951c851d
marcos-openclaw <marcos.openclaw@gmail.com>
2026-03-25 13:36:04 +0100

    Fix: remove duplicate version catalog declaration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,11 +19,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
+
 }
 
 rootProject.name = "RoundCutter"
